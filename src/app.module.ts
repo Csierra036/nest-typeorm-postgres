8334d8225efb37be8054f30cc1612a2ef2db508a
Cristian Sierra <cristiansierra.rap@gmail.com>
2025-04-28 10:39:32 -0400

    fix: Creation of a city

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { CitiesModule } from './cities/cities.module';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
-        entities: [join(process.cwd(), 'dist/**/*.entity.entity.js')],
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
         synchronize: true,
       }),
     }),
--- src/cities/dto/create-city.dto.ts
+++ src/cities/dto/create-city.dto.ts
@@ -1,5 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 export class CreateCityDto {
+    @ApiProperty({
+        example: 'Caracas',
+        description: 'Name of the city',
+    })
     name: string;
+
+    @ApiProperty({
+        example: 'Capital city of Venezuela',
+        description: 'Description of the city',
+    })
     description: string;
+
+    @ApiProperty({
+        example: true,
+        description: 'Is the city active?',
+    })
     active: boolean;
 }
