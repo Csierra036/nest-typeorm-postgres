f2470be5cf1c2bcc6eaeb5f8df7b3c21af8c69bc
Cristian Sierra <cristiansierra.rap@gmail.com>
2025-04-28 10:04:31 -0400

    feat: CRUD

---
+++ src/cities/cities.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { CitiesService } from './cities.service';
+import { CitiesController } from './cities.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { City } from './entities/city.entity';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([City]) // <<--- Aquí registras tu Entity
+  ],
+  controllers: [CitiesController],
+  providers: [CitiesService],
+  exports: [CitiesService],
+})
+export class CitiesModule {}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Postgre Nest TypeOrm')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 8000);
+}
+bootstrap();
